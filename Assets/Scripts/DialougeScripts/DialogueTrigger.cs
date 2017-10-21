c02ab9b28fc1ab0df6f3527dad5fe3b9159122ed
LamNg <LamNg96@gmail.com>
2017-10-20 19:55:03 -0700

    Add files via upload

---
+++ Assets/Scripts/DialougeScripts/DialogueTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ViewModel : MonoBehaviour {
+
+    //when start button is pressed
+    public void buttonStartGame() {
+        //change scene when pressed
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void buttonStorySelect()
+    {
+        string buttonName = this.name;
+        SceneManager.LoadScene(buttonName);
+    }
+
+    public void buttonCorrectAnswer()
+    {
+        string buttonName = this.name;
+        SceneManager.LoadScene(buttonName);
+    }
+
+    public void buttonLose()
+    {
+        //change scene when pressed
+        SceneManager.LoadScene("End1");
+    }
+
+    public void buttonMain()
+    {
+        //change scene when pressed
+        SceneManager.LoadScene("Main Menu");
+    }
+}
