afbac7202119b42c5ae13a0213f354e88e1c7ade
Brandon Truong <Brandon.t1995@hotmail.com>
2017-11-14 01:57:07 -0800

    changed back to per-session
    -text file is changed back to per-session
    -only use the last 4 lines in text file.

--- Assets/Scripts/PlayerLog.cs
+++ Assets/Scripts/PlayerLog.cs
@@ -10,20 +10,17 @@ public class PlayerLog : MonoBehaviour {
     public static int ShopFirst;
     public static int CreditsFirst;
 
-    bool firstRun = false;
+    bool firstRun = true;
 
-	// Use this for initialization
+    // Use this for initialization
+
 	void Start () {
-        StartFirst = PlayerPrefs.GetInt("StartFirst");
-        ShopFirst = PlayerPrefs.GetInt("ShopFirst");
-        CreditsFirst = PlayerPrefs.GetInt("CreditsFirst");
+        //StartFirst = PlayerPrefs.GetInt("StartFirst");
+        //ShopFirst = PlayerPrefs.GetInt("ShopFirst");
+        //CreditsFirst = PlayerPrefs.GetInt("CreditsFirst");
+        print("current value of Bool: " + firstRun);
     }
     
-    void Awake()
-    {
-        firstRun = true;
-    }
-
     public void countChoice(string Button)
     {
         if (firstRun)
@@ -31,29 +28,29 @@ public class PlayerLog : MonoBehaviour {
             if (Button == "Start")
             {
                 StartFirst += 1;
-                PlayerPrefs.SetInt("StartFirst", StartFirst);
+                //PlayerPrefs.SetInt("StartFirst", StartFirst);
             }
             else if (Button == "Shop")
             {
                 ShopFirst += 1;
-                PlayerPrefs.SetInt("ShopFirst", ShopFirst);
+                //PlayerPrefs.SetInt("ShopFirst", ShopFirst);
             }
             else if (Button == "Credits")
             {
                 CreditsFirst += 1;
-                PlayerPrefs.SetInt("CreditsFirst", CreditsFirst);
+                //PlayerPrefs.SetInt("CreditsFirst", CreditsFirst);
             }
-            firstRun = false;
             SplashScreenFirstChoiceWrite();
-            Debug.Log(firstRun);
+            
         }
-
+        firstRun = false;
+        Debug.Log(firstRun);
     }
 
     public void SplashScreenFirstChoiceWrite()
     {
         string text = "Player Selected First: \nStart: " + StartFirst+ " \nShop: " + ShopFirst + "\nCredits: " + CreditsFirst;
-        TextWriter textWriter = new StreamWriter(@"SplashScreenDecision.txt", false);
+        TextWriter textWriter = new StreamWriter(@"SplashScreenDecision.txt", true);
         textWriter.WriteLine(text);
         textWriter.Close();
     }
