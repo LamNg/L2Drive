adf04a27985ae792163ae570dd8a59c219783c23
Brandon Truong <Brandon.t1995@hotmail.com>
2017-11-14 00:22:35 -0800

    Testing some stuff for splash screen count
    -trying to test boolean to only track first press of session

--- Assets/Scripts/PlayerLog.cs
+++ Assets/Scripts/PlayerLog.cs
@@ -1,25 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
 
 public class PlayerLog : MonoBehaviour {
 
-    public static int StartFirst;
-    public static int ShopFirst;
-    public static int CreditFirst;
+    public int StartFirst;
+    public int ShopFirst;
+    public int CreditFirst;
+
+    bool firstRun;
 
     public int value = 5;
 	// Use this for initialization
 	void Start () {
         bool firstRun = true;
         Debug.Log(value);
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    }
+
+    public void countChoice()
+    {
+        if (firstRun)
+        {
+            if (this.name == "Start")
+            {
+                StartFirst += 1;
+            }
+            else if (this.name == "Shop")
+            {
+                ShopFirst += 1;
+            }
+            else if (this.name == "Credits")
+            {
+                CreditFirst += 1;
+            }
+            firstRun = false;
+        }
+
+    }
+
+    public void SplashScreenFirstChoiceWrite()
+    {
+        string text = "Player Selected First: \nStart: " + StartFirst+ " \nShop: " + ShopFirst + "\nCredits: " + CreditFirst;
+        TextWriter textWriter = new StreamWriter(@"SplashScreenDecision.txt", true);
+        textWriter.WriteLine(text);
+        textWriter.Close();
+        print(text);
+    }
+
+    public void Update()
+    {
+        Debug.Log(StartFirst);
+        Debug.Log(ShopFirst);
+        Debug.Log(CreditFirst);
+    }
 
 
 
